3de1d5eeea59828cb34201ffaa736e7760168447
Akshat Mittal <akshatmittal2024@gmail.com>
2025-11-13 18:37:21 +0530

    changed paths

--- Code/Server/Frontend/src/Lib/axios.js
+++ Code/Server/Frontend/src/Lib/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://127.0.0.1:5000/api" : "/api",
+    baseURL: import.meta.env.MODE === "development" ? "http://127.0.0.1:5000/api" : "https://skainet.onrender.com/api",
 });
